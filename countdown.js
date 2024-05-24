173b9aed875270cbc3b10183c90b0c0ff781bbc3
Ankura <Master-Anku.com>
2024-05-24 20:38:05 +0630

    countdown

---
+++ countdown.js
@@ -0,0 +1,18 @@
+// Write code to print all numbers from `num` down to 1
+// You may assume `num` will be a positive number
+
+var countdown = function(num) {
+  // num ဆိုတဲ့နံပါတ်ကနေ စတင်ဖို့ဖြစ်ပါတယ်။
+  for (let i = num; i >= 1; i--) {
+    // i နံပါတ်ကို console မှာ ပြပါ။
+    console.log(i);
+};
+};
+
+// function countdown(num) {
+//     for (let i = num; i >= 1; i--) {
+//       console.log(i);
+//     }
+//   };
+
+ 
